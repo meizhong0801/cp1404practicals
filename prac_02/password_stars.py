3bad606af4c5e51044df504ecc4545fb8720a849
meizhong0801 <mei.zhong@my.jcu.edu.au>
2023-03-26 11:54:24 +0800

    First commit password_stars.py

---
+++ prac_02/password_stars.py
@@ -0,0 +1,31 @@
+"""
+Now let's modify this program.
+
+Move all the code inside a main() function and call main() at the bottom. Run it to make sure it works.
+Note: if you don't have a main function, the refactoring below will use global variables.
+So, it's an important first step to use main before adding other functions.
+
+"""
+
+MINIMUM_LENGTH = 8
+
+
+def main():
+    password = input("Enter your password: ")
+    while len(password) < MINIMUM_LENGTH:
+        print("The password length is too short")
+        password = input("Enter your password: ")
+    print("*" * len(password))
+
+main()
+
+
+# Test
+# Enter your password: 3
+# The password length is too short
+# Enter your password: 44446677
+# ********
+# Enter your password: 0
+# The password length is too short
+# Enter your password: 123456789000
+# ************

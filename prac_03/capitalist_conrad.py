43efdb70d4012e7775ac8e40046cedffa985c0b4
meizhong0801 <mei.zhong@my.jcu.edu.au>
2023-03-31 22:56:05 +0800

    Update capitalist_conrad.py to github.

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,16 +9,19 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1
+MAX_PRICE = 100
 INITIAL_PRICE = 10.0
-NUM_DAYS = 0
+OUTPUT_FILE = "result.txt"
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
-
+# Open OUTPUT_FILE(result.txt) if the file exists, create OUTPUT_FILE(result.txt) if the file doesn't exist.
+# Mode is write.
+out_file = open(OUTPUT_FILE, 'w')
+print(f"Starting price: ${price:,.2f}", file=out_file)
+num_days = 0
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
     # generate a random integer of 1 or 2
@@ -31,16 +34,8 @@ while MIN_PRICE <= price <= MAX_PRICE:
         # generate a random floating-point number
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
-
+    num_days += 1
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {num_days} price is: ${price:,.2f}", file=out_file)
 
-    for day in range(1, NUM_DAYS+1):
-        price_change = 0
-        if random.randint(0, 1) == 1:
-            price_change = random.uniform(0, MAX_INCREASE)
-        else:
-            price_change = -random.uniform(0, MAX_DECREASE)
-            price *= (1 + price_change)
-            price = round(price, 2)
-            print(f"On day {NUM_DAYS} price is ${price: .2f}")
+out_file.close()  # close file.

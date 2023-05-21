ced5bd3e112867f9c43c6319e428770220b2bb2d
meizhong0801 <mei.zhong@my.jcu.edu.au>
2023-05-21 14:06:03 +0800

    finished

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,16 @@
+from prac_09.unreliable_car import UnreliableCar
+
+
+def run_tests():
+    """Run simple tests/demos on Unreliable_car class."""
+    my_unreliable_car = UnreliableCar("Prius 1", 100, 50.3)
+    my_unreliable_car.drive(40)
+    print(my_unreliable_car)
+
+
+if __name__ == "__main__":
+    run_tests()
+
+
+# Prius 1, fuel=100, odometer=0
+# Prius 1, fuel=60, odometer=40
